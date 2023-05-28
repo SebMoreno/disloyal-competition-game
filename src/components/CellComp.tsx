ce2d31c73b35495822d6e47a94e9905841967844
Sebastián <juansebasmorenoruiz@gmail.com>
2023-05-28 00:36:14 -0500

    fixing highlight behavior when cell prop is undefined

--- src/components/CellComp.tsx
+++ src/components/CellComp.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import "../styles/Cell.css";
 import { Cell, Position } from "../types";
 import { HexagonSVG } from "./HexagonSVG.tsx";
@@ -24,10 +24,20 @@ const aspectRatio = 174 / 200;
 
 
 export const CellComp: React.FC<CellProps> = ({height, cell, onSelected, position}) => {
-    const [highlight, setHighlight] = useState(cell.isHighlighted);
+    const [highlight, setHighlight] = useState<boolean>(Boolean(cell.isHighlighted));
+    const [highlightOnHover, setHighlightOnHover] = useState<boolean>(Boolean(cell.isHighlighted));
     const width = height * aspectRatio;
     const handleClick: React.MouseEventHandler<HTMLDivElement> = useCallback(() => onSelected(position), [onSelected, position]);
 
+    useEffect(() => {
+        if (cell.isHighlighted === undefined) {
+            setHighlightOnHover(true);
+            setHighlight(false);
+        } else {
+            setHighlightOnHover(false);
+            setHighlight(cell.isHighlighted);
+        }
+    }, [cell]);
     if (cell.type === "none") {
         return <div className="cell">
             <HexagonSVG height={height} width={width} fill={colors[cell.type]}/>
@@ -36,8 +46,8 @@ export const CellComp: React.FC<CellProps> = ({height, cell, onSelected, positio
     return (
         <div className={"cell" + (highlight ? " highlight" : "")}
              onClick={handleClick}
-             onMouseOver={cell.isHighlighted === undefined ? (() => setHighlight(true)) : undefined}
-             onMouseLeave={cell.isHighlighted === undefined ? (() => setHighlight(false)) : undefined}>
+             onMouseOver={highlightOnHover ? (() => setHighlight(true)) : undefined}
+             onMouseLeave={highlightOnHover ? (() => setHighlight(false)) : undefined}>
             <HexagonSVG height={height} width={width} fill={colors[cell.type]}/>
             <div className="cell_content">
                 {cell.content.map((e, i) => <div key={i} className={e}/>)}
