6bc8adf966e13dfbd998058b79f9ab0ee7c3e683
Sebastián <juansebasmorenoruiz@gmail.com>
2023-06-02 00:06:15 -0500

    adding movements to player card

--- src/components/PlayerCard.tsx
+++ src/components/PlayerCard.tsx
@@ -5,14 +5,16 @@ import "../styles/PlayerCard.css";
 export interface PlayerCardProps {
     playerNumber: number;
     isCurrent: boolean;
+    movements: number;
     playerIdeas?: GameEvent[];
     playerCounters?: GameEvent[];
 }
 
-export const PlayerCard: React.FC<PlayerCardProps> = ({playerNumber, isCurrent}) => {
+export const PlayerCard: React.FC<PlayerCardProps> = ({playerNumber, isCurrent, movements}) => {
     return (
         <section className={"playerCard" + (isCurrent ? " highlight" : "")}>
             <article>Proyect Manager {playerNumber + 1}</article>
+            <article>Movements Remaining {movements}</article>
         </section>
     );
 }
